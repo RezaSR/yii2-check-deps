f72a2eca7e7944adaccadd2ba33dbc87226cc1b4
Reza S.R <rsr_reza@yahoo.com>
2019-09-20 22:46:24 +0430

    - Add composer.json - Add initial class

---
+++ CheckDeps.php
@@ -0,0 +1,12 @@
+<?php
+namespace rsr\yii2\checkDeps;
+
+use yii\base\BaseObject;
+use yii\base\BootstrapInterface;
+
+class CheckDeps extends BaseObject implements BootstrapInterface
+{
+
+    public function bootstrap($app)
+    {}
+}
